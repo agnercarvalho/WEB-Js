d3e350309fa01565eec0acfc442e2e399fe1e566
Agner C <agnerf@gmail.com>
2023-05-16 20:02:54 -0300

    Socorro

---
+++ S16 - Ajax/ajax/server.js
@@ -0,0 +1,26 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.use(express.static('.'))   // pasta atual serve os arquivos estaticos
+app.use(bodyParser.urlencoded({ extended: true}))   //obedece requisições do tipo submit formulário e transforma em objeto
+app.use(bodyParser.json())  //aplica nas requisições JSON, transformando-as em objeto
+
+//  app.get('/teste', (req, res) => res.send('Ok')) respondera a requisições do tipo GET quando o caminho for ./teste  
+
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: function(req,file,callback){
+        callback(null,'./upload')
+    },
+    filename: function(req, file, callback){
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req,res) =>{
+}) 
+
+app.listen(8080,() => console.log("Executando..."))
